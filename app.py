6913597011b98ac2a02fdc7fcb5452ae762c3a40
Shipra <shipravashist18@gmail.com>
2025-09-12 23:55:15 +0530

    Add Complete DevSecOps project

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
+# Setup Prometheus metrics
+metrics = PrometheusMetrics(app)
+
 @app.route("/")
 def home():
-    return "Hello DevSecOps"
+    return "Hello DevSecOps 🚀"
+
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
